c2ce533b06aa9b03ff62da66eeba0c4fd3f5cd30
Robin Gralton <rgralton24@gmail.com>
2022-04-03 16:41:45 -0700

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,12 @@ package com.company;
 
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
-        int lastPlayed = 0;
-        String lastSelected;
 
         Song one = new Song("First Class");
         Song two = new Song("Willow");
@@ -27,43 +26,14 @@ public class Main {
 
         int choice = 1;
 
-
+        System.out.println(myPlayList.getSize());
         do {
 
             choice = myPlayList.menu();
 
             switch (choice) {
                 case 1:// Play all songs
-
-                    int selection = 0;
-                    for (int i = 0; i < (myPlayList.getSize() * 8); i++) {
-                        //selection =0;
-                        System.out.println(myPlayList.getIndex(i % (myPlayList.getSize()-1)));
-                        lastSelected = myPlayList.getIndex(i).getSongTitle();
-
-                        System.out.println("Continue playing y for \"yes\" n for \"no\"");
-                        selection = 0;
-                        if (keyboard.nextLine().equalsIgnoreCase("n")) {
-                            selection = myPlayList.playAllMenu();
-                            if (selection == 5)
-                                break;
-                        }
-                        switch (selection) {
-                            case 1: //play last selected
-                                System.out.println(myPlayList.playLastSelected(lastSelected));
-                                break;
-                            case 2: //overwrite the last playing song (next)
-                                myPlayList.overWrite(i % (myPlayList.getSize()-1));
-                                break;
-                            case 3: //repeat last played song
-                                myPlayList.loopLastPlayed(i % (myPlayList.getSize())-1);
-                                break;
-                            case 4: //repeat last selected song
-                                myPlayList.loopLastSelected(lastSelected);
-                                break;
-
-                        }
-                    }
+                        myPlayList.playAll();
 
                     break;
                 case 2: //add a song
@@ -84,24 +54,22 @@ public class Main {
 
                     System.out.println("What song title would you like to add? ");
                     title = keyboard.nextLine();
-                    ;
                     myPlayList.removeSong(myPlayList.getSong(title));
 
                     break;
                 case 5: //remove multiple song
-
                     myPlayList.removeMultiple();
                     break;
 
                 case 6:
                     myPlayList.shuffle();
-
                     break;
 
                 default:
+                    System.out.println("Thank you");
                     System.exit(0);
             }
         } while (choice > 0 && choice < 7);
-        System.out.println("Thank you");
+
     }
 }
--- src/com/company/Playlist.java
+++ src/com/company/Playlist.java
@@ -1,62 +1,93 @@
+//The playlist class makes a few important assumptions, that there is already songs in the list
+//Any Loop or repeat method has been modified to not create an infinite loop but still demonstrates the repetition of a song
+//Given the program is text based to continue playing the list we select 'y' to select a new option we use 'n' to generate a new
+//menu that can be selected from.
+
 package com.company;
-
 import java.util.*;
-
 public class Playlist {
-    //linkedList circular data structure
-    protected List<Song> myPlaylist;
+
+
+    private LinkedList<Song> myPlaylist;
+    private int currentSong;
     int size = 0;
+    private Song lastPlayed;
+    private Song lastSelected;
     Scanner keyboard = new Scanner(System.in);
 
-
+    /**
+     * constructor creates a new playlist with an empty LinkedList
+     */
     public Playlist() {
         myPlaylist = new LinkedList<>();
     }
 
-    public Playlist(List p) {
-
-        myPlaylist = p;
-    }
-
-    // take multiple songs and add to list
+    /**
+     *  adds a song to the back of the playlist and increases the size of the list
+     * @param s the song to be added
+     */
     public void addSong(Song s) {
         myPlaylist.add(s);
         size++;
     }
 
+    /**
+     * getSize
+     * @return int size
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * addMultiple - adds multiple songs to the playlist and increases the size once for each song added
+     */
     public void addMultiple() {
         String title = "";
-        Scanner keyboard = new Scanner(System.in);
+        //ask user for song title in a do while loop, user can add however many songs when they are done they type "done"
         do {
             System.out.println("Please enter the song title you'd like to add (enter done to exit) ");
-            title = keyboard.nextLine();
+            title = keyboard.nextLine().toLowerCase();
             if (title.equalsIgnoreCase("done"))
                 return;
-
+            //we add to size for each song
             size++;
+            //creat a new song objects
             Song newSong = new Song(title);
+            //and add that song to the playlist
             myPlaylist.add(newSong);
 
         } while (!title.equalsIgnoreCase("done"));
     }
 
-    //play last selected song
+    /**
+     * playLastSelected song - user must manually select a song to be played
+     * @param title the title of the song to be selected
+     * @return the selected song and set to lastSelected
+     */
     public Song playLastSelected(String title) {
-        return getSong(title);
+        //set current song to the index of the selected song, otherwise it reverts to the original place
+        currentSong = getIndex(title);
+        //assign last selected to title and return
+        return lastSelected = getSong(title);
 
     }
 
-    public void loopLastSelected(String title) {
+    /**
+     *loopLastSelected - loops the last selected song
+     * choose to not implement an infinite loop
+     */
+    public void loopLastSelected() {
         for (int i = 0; i < 15; i++) {
-            System.out.println(getSong(title));
+            System.out.println(lastSelected);
         }
     }
 
-
+    /**
+     * getSong - returns the song with specified title
+     * @param obj title of the song to be returned
+     * @return song
+     */
     public Song getSong(String obj) {
         for (Song s : myPlaylist) {
             if (s.title.equalsIgnoreCase(obj))
@@ -67,66 +98,96 @@ public class Playlist {
 
     }
 
-    //loop selected song - iterator should point to this song
-    public void repeatLastSelected(String obj) {
-        Song s = getSong(obj);
+    /**
+     * getIndex of song
+     * @param obj title of song to find the index of
+     * @return index of the obj
+     */
+    public int getIndex(String obj) {
         int count = 0;
-        while (count < 15) {
-            System.out.println(s);
+        for (Song s : myPlaylist) {
+            if (s.title.equalsIgnoreCase(obj))
+            break;
+
             count++;
+        }
+
+        return count;
+    }
+
+    /**
+     * overWrite - essentially playNext
+     */
+    public void overWrite() {
+        //play/printout the next song so currentSong +1 but since our playAll loop continues beyond the index of
+        //our playlist need to mod by the size
+        System.out.println(myPlaylist.get((currentSong % (getSize()) +1)));
+    }
+
+    //loop last played song
+    //take lastPlayedSong and loop
+
+    /**
+     * loopLastPlayed Song - loop through the last selected song
+     * again  choose not to implement an infinite loop
+     */
+    public void loopLastPlayed() {
+        for (int j = 0; j < 15; j++) {
+            System.out.println(lastPlayed);
+
         }
     }
 
-    //overwrite the last playing song..next/prev??
-    public void overWrite(int current) {
-        if (current == myPlaylist.size())
-            current = -1;
-
-        myPlaylist.get(current + 1);
-    }
-
-
-    //shuffle
+    /**
+     * shuffle - shuffles the playlist without altering its original order
+     */
     public void shuffle() {
-        ArrayList<Integer> prevPlay = new ArrayList<>(myPlaylist.size());
+        //using an array to track the random numbers generated
+        ArrayList<Integer> prevPlay = new ArrayList<>(getSize());
 
-        // Collections.shuffle(newList);
+
         Random rng = new Random();
-        int randIndex = rng.nextInt(myPlaylist.size());
-        //int lastRand = 0;
+        int randIndex;
 
-        for (int i = 0; i < myPlaylist.size(); i++) {
+        //loop through the size of the playlist
+        for (int i = 0; i < getSize(); i++) {
+            //generate a random number
             do {
-                randIndex = rng.nextInt(myPlaylist.size());
+                randIndex = rng.nextInt(getSize());
 
+            //check if the rng has already been used if so generate a new random number
             } while (prevPlay.contains(randIndex));
 
+            //print/play the song at the random number index
             System.out.println(myPlaylist.get(randIndex));
+
+            // add the successful rng to the array so its not used again
             prevPlay.add(randIndex);
 
         }
     }
 
-    //remove
+    /**
+     * removeSong - removes a song from the playlist
+     * @param song the object to be removed
+     * also must reduce the size of the list
+     */
     public void removeSong(Song song) {
         myPlaylist.remove(song);
         size--;
     }
 
 
-    public Song getIndex(int i) {
-        return myPlaylist.get(i);
-    }
-
-
-    //remove multiple
+    /**
+     * removeMultiple, allows the user to remove multiple songs at once
+     * similar to the logic of add multiple
+     */
     public void removeMultiple() {
         keyboard.nextLine();
         String title = "";
-        //Scanner keyboard = new Scanner(System.in);
         while (!title.equalsIgnoreCase("done")) {
             System.out.println("Please enter the song title to remove (enter done to exit) ");
-            title = keyboard.nextLine();
+            title = keyboard.nextLine().toLowerCase();
             if (title.equalsIgnoreCase("done"))
                 break;
             size--;
@@ -135,17 +196,66 @@ public class Playlist {
         }
     }
 
-    //loop last played song
-    //take lastPlayedSong and loop
-    public void loopLastPlayed(int i) {
-        for (int j = 0; j < 15; j++) {
-            System.out.println(myPlaylist.get(i) + " ");
+
+    /**
+     * PlayAll is the main driver of the playlist. Its used to play/print the songs
+     * Also provides the other options such as play last selected, overWrite current song,
+     * loop last played, and loop last selected
+     * given our limitations using a text format to continue playing the playlist we select 'y' or 'n' if
+     * we want to make a new selection.
+     */
+    public void playAll() {keyboard.nextLine();
+        for (int i = 0; i < (getSize()*8); i++) {
+            currentSong = i;
+            System.out.println(myPlaylist.get(i % (getSize())));
+
+            System.out.println("Continue playing y for \"yes\" n for \"no\"");
+            int selection = 0;
+            if (keyboard.nextLine().equalsIgnoreCase("n")) {
+                selection = playAllMenu();
+                if (selection == 5)
+                    break;
+            }
+            switch (selection) {
+                case 1: //play last selected
+                    keyboard.nextLine();
+                    System.out.println("Enter title of selected song ");
+                    String titleOfLastSelected = keyboard.nextLine().toLowerCase();
+                    System.out.println(playLastSelected(titleOfLastSelected));
+                    i = currentSong;
+                    break;
+                case 2: //overwrite the last playing song (next)
+                    keyboard.nextLine();
+                    overWrite();
+                    i+=1;
+                    break;
+                case 3: //repeat last played song
+                    keyboard.nextLine();
+                    lastPlayed = myPlaylist.get(i % (getSize()));
+                    loopLastPlayed();
+                    break;
+                case 4: //repeat last selected song
+                    keyboard.nextLine();
+                    if(lastSelected == null) {
+                        System.out.println("Error no previously selected song, please enter song title");
+                        String title = keyboard.nextLine().toLowerCase();
+                        lastSelected = getSong(title);
+                    }
+                    loopLastSelected();
+                    break;
+
+            }
+
+            lastPlayed = myPlaylist.get(i % (getSize()-1));
         }
     }
 
 
+    /**
+     * menu - is the main menu and returns an int to the main program to use in a switch statement
+     * @return int choice
+     */
     public int menu() {
-        //Scanner keyboard = new Scanner(System.in);
         int choice = 0;
         System.out.println("Please make a selection from below:");
         System.out.println("Enter 1 to Play all songs. ");
@@ -154,9 +264,10 @@ public class Playlist {
         System.out.println("Enter 4 to Remove a song. ");
         System.out.println("Enter 5 to remove multiple songs. ");
         System.out.println("Enter 6 to Shuffle songs. ");
+        System.out.println("Enter 7 to Exit. ");
 
         choice = keyboard.nextInt();
-        if (choice > 6 || choice < 1) {
+        if (choice > 7 || choice < 1) {
             System.out.println("Please enter a valid entry 1 -5.");
             menu();
         }
@@ -165,6 +276,10 @@ public class Playlist {
 
     }
 
+    /**
+     * playAll menu is speciic to what you can do while the playlist is playing.
+     * @return int choice
+     */
     public int playAllMenu() {
         int choice = 0;
         System.out.println("Please make a selection from below:");
@@ -181,20 +296,6 @@ public class Playlist {
         return choice;
     }
 
-    //loop all songs
-    public void playAll() {
-        //Scanner keyboard = new Scanner(System.in);
-        int choice = 0;
-        for (int i = 0; i < size; i++) {
-            System.out.println(myPlaylist.get(i));
-            System.out.println("Continue playing y for \"yes\" n for \"no\"");
-
-            if (keyboard.nextLine().equalsIgnoreCase("n")) {
-                return;
-            }
-
-        }
-    }
 
 
 }
